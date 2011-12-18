679e735a234c4407757a0dbe7a5cb3e848ebf8cc
Maxime B <max@debian.(none)>
2011-12-18 13:11:16 +0000

    Correct create_link script

--- create_link.sh
+++ create_link.sh
@@ -1,4 +1,8 @@
-ln -s dotfiles& .bashrc
-ln -s dotfiles& .taskrc
-ln -s dotfiles& .tigrc
-ln -s dotfiles& .tmux.conf
+for file in bashrc taskrc tigrc tmux.conf
+do
+  ln -s .dotfiles/$file .$file
+done
+#ln -s .dotfiles/bashrc bashrc
+#ln -s .dotfiles/taskrc taskrc
+#ln -s .dotfiles/tigrc tigrc
+#ln -s .dotfiles/tmux.conf tmux.conf
