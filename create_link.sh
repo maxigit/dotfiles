c4e32cd0e21d7e9eae1b8db10844be3c79812057
Maxime Bourget <bmx007@gmail.com>
2013-04-07 08:46:39 +0100

    Add link completion

--- create_link.sh
+++ create_link.sh
@@ -1,4 +1,4 @@
-for file in bashrc taskrc tigrc tmux.conf
+for file in bashrc taskrc tigrc tmux.conf task_completion.sh tmux_completion.bash tig-completion.bash
 do
   ln -s .dotfiles/$file .$file
 done
